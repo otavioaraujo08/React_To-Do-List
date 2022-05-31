c0076cc02346488b14da03ae2a012d532b96ce71
Otávio Araujo <51797012+otavioaraujo08@users.noreply.github.com>
2022-05-31 14:19:07 -0300

    Componente de listar

---
+++ src/components/todolist/index.jsx
@@ -0,0 +1,33 @@
+import * as React from 'react';
+import DeleteIcon from '@mui/icons-material/Delete';
+import { 
+  Paper, ListItem, ListItemButton, ListItemIcon, ListItemText,
+  Checkbox, IconButton
+} from '@mui/material'
+
+export default function Todolist({ todo, deleteTodo }) {
+  return (
+    <Paper>
+      <ListItem
+        secondaryAction={
+          <IconButton edge="end" aria-label="delete" onClick={() => deleteTodo(todo.id)}>
+            <DeleteIcon />
+          </IconButton>
+        }
+        disablePadding
+      >
+        <ListItemButton role={undefined} dense>
+          <ListItemIcon>
+            <Checkbox
+              edge="start"
+              tabIndex={-1}
+              disableRipple
+            />
+          </ListItemIcon>
+
+          <ListItemText primary={`${todo.id} - ${todo.text}`}/>
+        </ListItemButton>
+      </ListItem>
+    </Paper>
+  )
+}
