2c6bbad43c394b636aebbb035d310716061320a5
Otávio Araujo <51797012+otavioaraujo08@users.noreply.github.com>
2022-05-31 16:27:58 -0300

    Refatorando formulário

--- src/components/form/index.jsx
+++ src/components/form/index.jsx
@@ -1,27 +1,36 @@
-import { Paper, Button, TextField } from '@mui/material'
+import { Box, Paper, Button, TextField } from '@mui/material'
+import { styled } from '@mui/material/styles' 
 import { useState } from "react"
-import AddIcon from '@mui/icons-material/Add';
+import AddIcon from '@mui/icons-material/Add'
 
-function Form({ addTodo, }){
+const FormComponent = styled(Box)({
+    display: "flex", 
+    justifyContent: "center"
+})
+
+function Form({ addTodo }){
+    // O valor inicial sempre é nossa constane, e a proxima variável é uma função que altera o valor da primeira
     const [text, setText] = useState(null)
     const [id, setId] = useState(0)
     
-    const todoCreate = (text) => {
-        const todoObj = {text: text, id: id}
+    const todoCreate = (e) => {
+        e.preventDefault()
+
         setId(id + 1)
-        addTodo(todoObj)
-        
-        document.getElementById("outlined-basic").value = null;
+
+        addTodo({text, id})
+        // Para alterar o valor de text - usa-se o setText("O valor do text")
+        setText('')
     }
 
     return(
         <Paper style={{ padding: "1em" }}>
-            <div style={{ display: "flex", justifyContent: "center"}}>
+            <FormComponent onSubmit={(e) => todoCreate(e)} component="form">
                 {/* Input de Texto */}
                 <TextField 
-                  id="outlined-basic" 
+                  value={text}
                   label="Tarefa" 
-                  placeholder="Digite sua tarefa" 
+                  placeholder="Digite uma tarefa" 
                   variant="outlined" 
                   fullWidth
                   style={{ marginRight: "1em" }}  
@@ -30,13 +39,13 @@ function Form({ addTodo, }){
 
                 {/* Componente de botão */}
                 <Button 
+                  type="submit"
                   variant="contained"
                   size="large"
-                  onClick = {() => todoCreate(text)}
                 >
                     <AddIcon />
                 </Button>
-            </div>
+            </FormComponent>
         </Paper>
     )
 }
